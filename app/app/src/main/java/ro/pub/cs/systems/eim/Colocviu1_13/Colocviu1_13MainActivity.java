804d55f2f1e2ee7fa5cf6023a3352656ef2db8e6
Mihaela Catrina <mcatrina@bitdefender.com>
2020-04-13 10:55:56 +0300

    B.1 + B.2

--- app/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/Colocviu1_13MainActivity.java
+++ app/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/Colocviu1_13MainActivity.java
@@ -3,12 +3,64 @@ package ro.pub.cs.systems.eim.Colocviu1_13;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Colocviu1_13MainActivity extends AppCompatActivity {
+
+    private int noClicks = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_colocviu1_13_main);
+
+        Button northButton = (Button) findViewById(R.id.buttonNorth);
+        northButton.setOnClickListener(buttonClickListener);
+
+        Button southButton = (Button) findViewById(R.id.buttonSouth);
+        southButton.setOnClickListener(buttonClickListener);
+
+        Button eastButton = (Button) findViewById(R.id.buttonEast);
+        eastButton.setOnClickListener(buttonClickListener);
+
+        Button westButton = (Button) findViewById(R.id.buttonWest);
+        westButton.setOnClickListener(buttonClickListener);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(Constants.NO_CLICK_KEY, noClicks);
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        noClicks = savedInstanceState.getInt(Constants.NO_CLICK_KEY);
+    }
+
+
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+
+    private class ButtonClickListener implements Button.OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            noClicks++;
+            //Toast.makeText(getApplicationContext(), "No clicks: " + noClicks, Toast.LENGTH_SHORT).show();
+            EditText points_edit_text = (EditText) findViewById(R.id.points_edit_text);
+            String currentString = points_edit_text.getText().toString();
+
+            String currentButtonName = ((Button) view).getText().toString();
+
+            if (currentString.equals(""))
+                points_edit_text.setText(String.format("%s", currentButtonName));
+            else
+                points_edit_text.setText(String.format("%s, %s", currentString, currentButtonName));
+
+        }
     }
 }
---
+++ app/app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_13/Constants.java
@@ -0,0 +1,5 @@
+package ro.pub.cs.systems.eim.Colocviu1_13;
+
+public class Constants {
+    final public static String NO_CLICK_KEY = "NoClicksKey";
+}
